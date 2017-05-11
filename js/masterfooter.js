e6382624713fba79bbad81b339836146139563ee
TimmyToast <plumbt@toa.st>
2017-05-11 16:34:58 +0100

    master footer + stores JS

---
+++ js/masterfooter.js
@@ -0,0 +1,103 @@
+function triggerEmailPopup() {
+    thisHref = window.location.href;
+    var now = new Date();
+    var time = now.getTime();
+    time += 3600 * 1000;
+    now.setTime(time);
+
+    showQualifyer = thisHref.indexOf("Master+Group") == -1 && thisHref.indexOf("utm_medium=paidsocial&utm_campaign=highsummer") != -1 && !$.cookie("emailClickUserSummer") // ??????
+
+    if (showQualifyer) {
+      document.cookie = "emailClickUserSummer=true; expires=" + now.toUTCString() + "; path=/";
+      openSignupField()
+    }
+}
+
+function openSignupField() {
+    var scrollPos = $(window).scrollTop() + 120
+    $('.elb_Lightbox').height($(document).height())
+    $('.elb_Lightbox').html('<div class="elb_passwordBox" style="margin-top:' + scrollPos + 'px">' + $(".emailBox").html() + '</div>')
+    $('.elb_Lightbox').fadeIn()
+
+    $('.elb_Lightbox').on('click', function(e) {
+      if (e.target == this) {
+        closeemailBox()
+      }
+    })
+}
+
+function closeemailBox() {
+    $('.elb_Lightbox').fadeOut()
+    $('.elb_Lightbox').html('')
+}
+
+function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+}
+
+function sendGAevent(){
+ _gaq.push(["_trackEvent","New collection page email submit","Click","Sale page email submit"])
+}
+
+function submitEmail() {
+    submitedEmailAddress = $("#signupEmail").val()
+    if (isEmail(submitedEmailAddress)) {
+        $.ajax({
+            type: "GET",
+            url: "https://post.toast.co.uk/automated/action.jsp?action=updateRecipientNoMsg&errorPage=/automated/action.jsp&gid=750148124&uemail=apitoast@api.com&psw=Api321$123&pemail=" + submitedEmailAddress + "&self=true&namedattr_NewsletterOptin=TRUE&namedattr_SignupSource=New_Collection_Overlay",
+            dataType: "jsonp",
+            error: function(xhr, ajaxOptions, thrownError) {
+                $(".emailSender").html("<p>Thanks, we have received your email address.</p>")
+                closeemailBox()
+                sendGAevent()
+            }
+        })
+    } else {    
+   }
+}
+
+$(document).ready(function() {
+    $('body').prepend('<div class="elb_Lightbox"></div>')
+    setTimeout(triggerEmailPopup, 15000);
+
+});
+
+ 
+  
+var _gaq = _gaq || [];
+var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+_gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+_gaq.push(['_setAccount', 'UA-1341106-1']);
+_gaq.push(['_setDomainName', 'toa.st']);
+_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+  
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '195982334167063');
+fbq('track', 'PageView');
+                                                         
+var _prum = [['id', '58a5aa15f4bab53b1f7b23c6'],
+             ['mark', 'firstbyte', (new Date()).getTime()]];
+(function() {
+    var s = document.getElementsByTagName('script')[0]
+      , p = document.createElement('script');
+    p.async = 'async';
+    p.src = '//rum-static.pingdom.net/prum.min.js';
+    s.parentNode.insertBefore(p, s);
+})()
+                                                              
+
+
+  
