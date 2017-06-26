21c6263fd0af50301dc1d59d2e9757d1df61e7a9
TimmyToast <plumbt@toa.st>
2017-06-26 15:58:19 +0100

    Main Hidden

--- js/masterfunctions_c17i.js
+++ js/masterfunctions_c17i.js
@@ -1,4 +1,4 @@
-function flagSwap() {
+ flagSwap() {
     str = $(".menu__title").text(), rg = /[a-zA-Z]+/g, el = $("#footer-country-select .has-flag-uk").first(), el.html(el.html().replace(/GBP/gi, str.match(rg)[0])), currentFlag = $(".has-flag").attr("class").split(" ").pop(), $("#footerCountryFlag").removeClass("has-flag-uk"), $("#footerCountryFlag").addClass(currentFlag)
 }
 
