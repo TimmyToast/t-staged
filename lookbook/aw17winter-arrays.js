9f3e44c8178c00de7a7e012d1eaca6c9cca937a9
Tim Plumb <plumbt@toa.st>
2017-09-27 10:01:47 +0100

    lookbook

---
+++ lookbook/aw17winter-arrays.js
@@ -0,0 +1,301 @@
+aw17autumn = [{
+  products: [{
+  prodId: "FHIAG",
+  skuColor: "navy"
+  },{
+  prodId: "C4ICH",
+  skuColor: "darknavy"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_1.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_1.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "1",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "",
+  skuColor: ""
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_2.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_2.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "2",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "C7IAE",
+  skuColor: "putty"
+  },{
+  prodId: "C5ICD",
+  skuColor: "navy"
+  },{
+  prodId: "C4ICD",
+  skuColor: "navy"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_3.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_3.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "3",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "C7IAE",
+  skuColor: "putty"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_4.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_4.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "4",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "CAIBQ",
+  skuColor: "flannelgrey"      
+  },{
+  prodId: "C1ICD",
+  skuColor: "darknavy"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_5.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_5.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "5",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "",
+  skuColor: ""
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_6.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_6.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "6",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "C7ICJ",
+  skuColor: "blackecru"
+  },{
+  prodId: "CAIBK",
+  skuColor: "darknavyindigo"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_7.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_7.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "7",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "C7ICI",
+  skuColor: "white"
+  },{
+  prodId: "CAIBQ",
+  skuColor: "cumin"
+  },{
+  prodId: "C4ICE",
+  skuColor: "navy"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_8.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_8.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "8",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "C7IAI",
+  skuColor: "offwhite"
+  },{
+  prodId: "CAICB",
+  skuColor: "navy"
+  },{
+  prodId: "C3ICG",
+  skuColor: "goldcharcoal"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_9.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_9.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "9",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "C1ICO",
+  skuColor: "blackecru"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_10.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_10.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "10",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "CAIAS",
+  skuColor: "boysenberry"
+  },{
+  prodId: "C1ICL",
+  skuColor: "portred"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_11.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_11.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "11",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "FHIAG",
+  skuColor: "navy"
+  },{
+  prodId: "C4ICH",
+  skuColor: "darknavy"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_12.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_12.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "12",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "FHIAG",
+  skuColor: "navy"
+  },{
+  prodId: "C4ICH",
+  skuColor: "darknavy"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_13.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_13.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "13",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "6QEAE",
+  skuColor: "blackwhite"
+  },{
+  prodId: "6BIBA",
+  skuColor: "walnut"
+  },{
+  prodId: "6BIAG",
+  skuColor: "walnut"
+  },{
+  prodId: "6CIAD",
+  skuColor: "slate"
+  },{
+  prodId: "6CIAJ",
+  skuColor: "paleumber"
+  },{
+  prodId: "6CIAK",
+  skuColor: "cloud"
+  },{
+  prodId: "6CIAL",
+  skuColor: "natural"
+  },{
+  prodId: "6BIAT",
+  skuColor: "oak"
+  },{
+  prodId: "6BIAU",
+  skuColor: "black"
+  },{
+  prodId: "6BIAV",
+  skuColor: "white"
+  },{
+  prodId: "6KIAA",
+  skuColor: "clear"
+  },{
+  prodId: "6KIAB",
+  skuColor: "clear"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_14.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_14.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "14",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "6KIAA",
+  skuColor: "clear"
+  },{
+  prodId: "6KIAB",
+  skuColor: "clear"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_15.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_15.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "15",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "64TTH-64TTI",
+  skuColor: "ecrugraphite"
+  },{
+  prodId: "64TTA-64TTB-64TTC-64TTD",
+  skuColor: "ecrugraphite"
+  },{
+  prodId: "63IAF",
+  skuColor: "oyster"
+  },{
+  prodId: "6QIAB",
+  skuColor: "tobacco"
+  },{
+  prodId: "6KIAA",
+  skuColor: "clear"
+  },{
+  prodId: "6CIAD",
+  skuColor: "slate"
+  },{
+  prodId: "6CIAJ",
+  skuColor: "paleumber"
+  },{
+  prodId: "6CIAK",
+  skuColor: "cloud"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_16.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_16.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "16",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "64TTH-64TTI",
+  skuColor: "ecrugraphite"
+  },{
+  prodId: "64TTA-64TTB-64TTC-64TTD",
+  skuColor: "ecrugraphite"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_17.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_17.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "17",
+  float: "left" 
+  },{
+  products: [{
+  prodId: "CDIAA",
+  skuColor: "redmulti"
+  },{
+  prodId: "CHIAO",
+  skuColor: "redmulti"
+  },{
+  prodId: "6QIAB",
+  skuColor: "tobacco"
+  }],
+  imgThumbSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/950/TOAST_Autumn_Lookbook_18.jpg",
+  imgSrc: "https://d117fiyhpld8f9.cloudfront.net/content-images/aw17/700/TOAST_Autumn_Lookbook_18.jpg",
+  orientation: "portrait",
+  percentWidth: "50",
+  lookbookID: "18",
+  float: "left" 
+  }];
